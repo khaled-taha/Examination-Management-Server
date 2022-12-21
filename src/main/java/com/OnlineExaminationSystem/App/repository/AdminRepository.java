9ede1ea2dc66062d3433cdc70af1ca2e40548741
Abdallah Ahmed <113655076+AbdallahAhmed0@users.noreply.github.com>
2022-12-21 22:27:09 +0200

    Update AdminRepository.java

--- src/main/java/com/OnlineExaminationSystem/App/repository/AdminRepository.java
+++ src/main/java/com/OnlineExaminationSystem/App/repository/AdminRepository.java
@@ -2,6 +2,8 @@ package com.OnlineExaminationSystem.App.repository;
 import com.OnlineExaminationSystem.App.entity.users.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
 
 import java.util.Optional;
 

aeb900a8ed6d47c7908317d90546eb99864dd8bd
Abdallah Ahmed <113655076+AbdallahAhmed0@users.noreply.github.com>
2022-12-21 22:59:09 +0200

    Update UserRepository.java

--- src/main/java/com/OnlineExaminationSystem/App/repository/UserRepository.java
+++ src/main/java/com/OnlineExaminationSystem/App/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.OnlineExaminationSystem.App.entity.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 
 import java.util.Optional;

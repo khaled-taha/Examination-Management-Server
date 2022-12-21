d1c2c667afe37de503fde5933dc9f6916565e950
Abdallah Ahmed <113655076+AbdallahAhmed0@users.noreply.github.com>
2022-12-22 00:26:22 +0200

    Update StudentRepository.java

--- src/main/java/com/OnlineExaminationSystem/App/repository/StudentRepository.java
+++ src/main/java/com/OnlineExaminationSystem/App/repository/StudentRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
      Optional<Student> findStudentById(Long studentId);
-     Optional<Student> findStudentByUniversityIdAndIdNot(long id, long universityId);
+     Optional<Student> findStudentByUniversityIdAndIdNot(long universityId, long id);
      void deleteById(Long studentId);
 
 

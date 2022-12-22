38ed1544ebd0a402caa095e06159fb2a9d5828b9
Abdallah Ahmed <113655076+AbdallahAhmed0@users.noreply.github.com>
2022-12-22 02:07:48 +0200

    Update StudentService.java

--- src/main/java/com/OnlineExaminationSystem/App/service/StudentService.java
+++ src/main/java/com/OnlineExaminationSystem/App/service/StudentService.java
@@ -19,12 +19,12 @@ public class StudentService {
 
     public Student addAndUpdateStudent(Student student) {
 
-            if(this.userRepository.findUserByEmailContainingAndIdNot(student.getEmail(), student.getId()).isPresent())
+            if(this.userRepository.findUserByEmailAndIdNot(student.getEmail(), student.getId()).isPresent())
                 throw new ApiException("Duplicate Email");
-            else if(this.studentRepository.findStudentByUniversityIdAndIdNot( student.getUniversityId(), student.getId()).isPresent())
+            else if(this.studentRepository.findStudentByUniversityIdAndIdNot(student.getUniversityId(), student.getId()).isPresent())
                 throw new ApiException("Duplicate UniversityId");
-        
-        this.studentRepository.save(student);
+            
+            this.studentRepository.save(student);
 
         return student;
     }

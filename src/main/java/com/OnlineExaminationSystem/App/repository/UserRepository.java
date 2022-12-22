7b4ba5feec99fca8dd26fe9590125ba2e48f94c1
Abdallah Ahmed <113655076+AbdallahAhmed0@users.noreply.github.com>
2022-12-22 02:17:12 +0200

    Update UserRepository.java

--- src/main/java/com/OnlineExaminationSystem/App/repository/UserRepository.java
+++ src/main/java/com/OnlineExaminationSystem/App/repository/UserRepository.java
@@ -3,15 +3,12 @@ package com.OnlineExaminationSystem.App.repository;
 import com.OnlineExaminationSystem.App.entity.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findUserByEmailContainingAndIdNot(String Email, long id);
+    Optional<User> findUserByEmailAndIdNot(String email, Long id);
+
 }

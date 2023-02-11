be26d29056c4d8c8bbe5366e010486e3e00b1b55
Khaled Taha <khaledtaha105@gmail.com>
2023-02-11 15:11:43 +0200

    A

---
+++ src/main/java/com/OnlineExaminationSystem/App/service/StudentService.java
@@ -0,0 +1,50 @@
+package com.OnlineExaminationSystem.App.service;
+
+import com.OnlineExaminationSystem.App.entity.users.Student;
+import com.OnlineExaminationSystem.App.exceptions.ApiException;
+import com.OnlineExaminationSystem.App.repository.StudentRepository;
+import com.OnlineExaminationSystem.App.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+    private final UserRepository userRepository;
+
+
+
+    public Student addAndUpdateStudent(Student student) {
+
+            if(this.userRepository.findUserByEmailAndIdNot(student.getEmail(), student.getId()).isPresent())
+                throw new ApiException("Duplicate Email");
+            else if(this.studentRepository.findStudentByUniversityIdAndIdNot(student.getUniversityId(), student.getId()).isPresent())
+                throw new ApiException("Duplicate UniversityId");
+
+            this.studentRepository.save(student);
+
+        return student;
+    }
+
+    public void deleteById(Long studentId) {
+        Student student = this.studentRepository.findStudentById(studentId)
+                .orElseThrow(() -> new ApiException("Student not found"));
+        this.studentRepository.deleteById(studentId);
+    }
+
+    public List<Student> getAllStudents() {
+        return this.studentRepository.findAll();
+    }
+
+    public Student getStudentById(Long studentId) {
+        Student student = this.studentRepository.findStudentById(studentId)
+                .orElseThrow(() -> new ApiException("Student not found"));
+        return student;
+    }
+
+
+}

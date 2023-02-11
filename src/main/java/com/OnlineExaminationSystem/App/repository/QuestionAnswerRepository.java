cc88b54001d9e89ca9ee641d4745c644ed821ac9
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2023-02-11 15:09:53 +0200

    Delete src directory

--- src/main/java/com/OnlineExaminationSystem/App/repository/QuestionAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.OnlineExaminationSystem.App.repository;
-
-import com.OnlineExaminationSystem.App.entity.Exam.QuestionAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, Long> {
-}

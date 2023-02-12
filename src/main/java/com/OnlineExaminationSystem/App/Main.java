2ff665a1e85fd8792a5055eee234ad898b97a41f
Khaled Taha <khaledtaha105@gmail.com>
2023-02-12 04:03:17 +0200

    Update

--- src/main/java/com/OnlineExaminationSystem/App/Main.java
+++ src/main/java/com/OnlineExaminationSystem/App/Main.java
@@ -1,13 +1,12 @@
 package com.OnlineExaminationSystem.App;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineExaminationSystemAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineExaminationSystemAppApplication.class, args);
-	}
-
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }

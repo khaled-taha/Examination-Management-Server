8cd2a5feff4c1135fe915dff7508ad1b0dfd40c0
Khaled Taha <khaledtaha105@gmail.com>
2023-02-12 03:08:21 +0200

    Update

--- src/main/java/com/OnlineExaminationSystem/App/repository/ExamResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.OnlineExaminationSystem.App.repository;
-
-import com.OnlineExaminationSystem.App.entity.Exam.ExamResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExamResultRepository extends JpaRepository<ExamResult, Long> {
-
-    ExamResult getExamResultByExamAttemptId(long examAttemptId);
-}
--- src/main/java/com/OnlineExaminationSystem/App/repository/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.OnlineExaminationSystem.App.repository;
-
-
-import com.OnlineExaminationSystem.App.entity.Exam.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
-}

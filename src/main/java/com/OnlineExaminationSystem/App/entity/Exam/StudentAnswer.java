c5322778e70ef3abd13a2c7354545ec7788ff806
Khaled Taha <khaledtaha105@gmail.com>
2023-02-12 04:06:47 +0200

    Update

---
+++ src/main/java/com/OnlineExaminationSystem/App/entity/Exam/StudentAnswer.java
@@ -0,0 +1,34 @@
+package com.OnlineExaminationSystem.App.entity.Exam;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student_answer")
+@NoArgsConstructor
+@Setter
+@Getter
+public class StudentAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "question_answer_id", nullable = false)
+    private QuestionAnswer questionAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "examAttempt_id")
+    @JsonIgnore
+    private ExamAttempt examAttempt;
+
+    @Column(name = "score")
+    private double points;
+
+    // Getters and setters
+}

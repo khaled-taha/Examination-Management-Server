f40bd223940cf00783ee466d85d80415a7fc13d2
Abdallah Ahmed <113655076+AbdallahAhmed0@users.noreply.github.com>
2023-02-26 15:41:17 +0200

    Update ExamAttempt.java

--- src/main/java/com/OnlineExaminationSystem/App/entity/Exam/ExamAttempt.java
+++ src/main/java/com/OnlineExaminationSystem/App/entity/Exam/ExamAttempt.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;

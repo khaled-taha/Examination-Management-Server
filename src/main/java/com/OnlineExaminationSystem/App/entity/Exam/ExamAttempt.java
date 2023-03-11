9c782c78d525abab5d18c47fa08f8eb559e64ebf
Abdallah Ahmed <113655076+AbdallahAhmed0@users.noreply.github.com>
2023-03-11 02:41:07 +0200

    Update ExamAttempt.java

--- src/main/java/com/OnlineExaminationSystem/App/entity/Exam/ExamAttempt.java
+++ src/main/java/com/OnlineExaminationSystem/App/entity/Exam/ExamAttempt.java
@@ -11,6 +11,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 @Table(name = "ExamAttempt", schema = "public")
 @NoArgsConstructor
 @Setter
